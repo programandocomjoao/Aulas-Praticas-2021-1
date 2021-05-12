db9e4c403d3e56841a811dfb4247ec5247796f15
programandocomjoao <46997033+programandocomjoao@users.noreply.github.com>
2021-05-12 12:53:33 -0300

    Add files via upload

---
+++ PPSW(422-20) TER Noite/2021-05-11/engenhariadesoftware.js
@@ -0,0 +1,11 @@
+function informarData() {
+	var data = Date();
+	document.write(data);
+}
+
+function exibirResolucao() {
+	var largura = screen.width;
+	var altura = screen.height;
+
+	document.write(largura + 'x' + altura);
+}
